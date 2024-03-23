e957032538e854bb0d765d62088300b5f17c64de
Sabian Hibbs <Sabian.hibbs@gmail.com>
2024-03-23 13:05:09 +0000

    added ssh helper

---
+++ ssh-helper.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Function to display error message and exit
+display_error() {
+    echo "Error: $1"
+    exit 1
+}
+
+# Check if the required arguments are provided
+if [ $# -ne 4 ]; then
+    display_error "Invalid number of arguments. Usage: script.sh -e example@example.com"
+fi
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -e)
+            email="$2"
+            shift
+            shift
+            ;;
+        *)
+            display_error "Invalid argument: $key"
+            ;;
+    esac
+done
+
+# Check if the required arguments are provided
+if [ -z "$email" ]; then
+    display_error "Missing required arguments. Usage: script.sh -e example@example.com"
+fi
+
+
+ssh-keygen ed25519 -C $email || display_error "Failed to create key"
+
+eval "$(ssh-agent -s)"
+
+ssh-add ~/.ssh/id_ed25519
+
+cat ~/.ssh/id_ed25519 | xclip -sel clip
+
+echo "Use this string with github login SSH"
